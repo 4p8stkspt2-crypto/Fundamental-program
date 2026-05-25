e6ce54ad4856d39ec008e852416d63c29332ab6e
4p8stkspt2-crypto <4p8stkspt2@privaterelay.appleid.com>
2026-05-25 11:37:26 +0700

    Add files via upload

---
+++ 1chieuexercises7.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MangMotChieu
+{
+    static void Main1(string[] args)
+    {
+
+        Console.Write("Nhập n và các phần tử: ");
+        int n1 = int.Parse(Console.ReadLine());
+        int[] arr1 = NhapMangNguyen(n1);
+        Bai01_XuatMang(arr1);
+
+        Console.Write("Nhập n và các phần tử số thực: ");
+        int n2 = int.Parse(Console.ReadLine());
+        double[] arr2 = NhapMangThuc(n2);
+        Bai02_TongVaTrungBinhCong(arr2);
+
+        Bai03_TimMaxMin(arr1);
+
+        Bai04_DemChanLe(arr1);
+
+        Console.Write("Nhập số x cần tìm: ");
+        int x5 = int.Parse(Console.ReadLine());
+        Bai05_TimKiem(arr1, x5);
+
+        Bai06_DaoNguocMang(arr1);
+
+        Bai07_KiemTraDoiXung(arr1);
+
+        Bai08_LietKeSoNguyenTo(arr1);
+
+        Bai09_TachMangChanLe(arr1);
+
+        Bai10_SapXepTangGiam(arr1);
+
+        Console.Write("Nhập số x cần chèn: ");
+        int x11 = int.Parse(Console.ReadLine());
+        Bai11_ChenPhanTu(arr1, x11); // arr1 lúc này đã được sắp xếp tăng từ Bài 10
+
+        Console.Write("Nhập vị trí k cần xóa (0 <= k < n): ");
+        int k12 = int.Parse(Console.ReadLine());
+        Bai12_XoaPhanTu(arr1, k12);
+
+        Bai13_DemTanSuat(arr1);
+
+        Bai14_XuatHienNhieuNhat(arr1);
+    }
+
+    static int[] NhapMangNguyen(int n)
+    {
+        int[] arr = new int[n];
+        string[] tokens = Console.ReadLine().Split(' ');
+        for (int i = 0; i < n; i++) arr[i] = int.Parse(tokens[i]);
+        return arr;
+    }
+
+    static double[] NhapMangThuc(int n)
+    {
+        double[] arr = new double[n];
+        string[] tokens = Console.ReadLine().Split(' ');
+        for (int i = 0; i < n; i++) arr[i] = double.Parse(tokens[i]);
+        return arr;
+    }
+
+    static void Bai01_XuatMang(int[] arr)
+    {
+        Console.WriteLine(string.Join(" ", arr));
+    }
+
+    static void Bai02_TongVaTrungBinhCong(double[] arr)
+    {
+        double tong = 0;
+        foreach (double x in arr) tong += x;
+        double tbc = tong / arr.Length;
+        Console.WriteLine($"{tong:F2} {tbc:F2}");
+    }
+
+    static void Bai03_TimMaxMin(int[] arr)
+    {
+        int max = arr[0], min = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+        Console.WriteLine($"{max} {min}");
+    }
+
+    static void Bai04_DemChanLe(int[] arr)
+    {
+        int chan = 0, le = 0;
+        foreach (int x in arr)
+        {
+            if (x % 2 == 0) chan++;
+            else le++;
+        }
+        Console.WriteLine($"{chan} {le}");
+    }
+
+    static void Bai05_TimKiem(int[] arr, int x)
+    {
+        int vt = -1;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == x)
+            {
+                vt = i;
+                break;
+            }
+        }
+        Console.WriteLine(vt);
+    }
+
+    static void Bai06_DaoNguocMang(int[] arr)
+    {
+        int[] tempArr = (int[])arr.Clone(); 
+        int i = 0, j = tempArr.Length - 1;
+        while (i < j)
+        {
+            int temp = tempArr[i];
+            tempArr[i] = tempArr[j];
+            tempArr[j] = temp;
+            i++; j--;
+        }
+        Console.WriteLine(string.Join(" ", tempArr));
+    }
+
+    static void Bai07_KiemTraDoiXung(int[] arr)
+    {
+        bool doiXung = true;
+        int n = arr.Length;
+        for (int i = 0; i < n / 2; i++)
+        {
+            if (arr[i] != arr[n - 1 - i])
+            {
+                doiXung = false;
+                break;
+            }
+        }
+        Console.WriteLine(doiXung ? "YES" : "NO");
+    }
+
+    static bool LaSoNguyenTo(int n)
+    {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+    static void Bai08_LietKeSoNguyenTo(int[] arr)
+    {
+        List<int> snt = new List<int>();
+        foreach (int x in arr)
+        {
+            if (LaSoNguyenTo(x)) snt.Add(x);
+        }
+        if (snt.Count == 0) Console.WriteLine("EMPTY");
+        else Console.WriteLine(string.Join(" ", snt));
+    }
+
+    static void Bai09_TachMangChanLe(int[] arr)
+    {
+        List<int> chan = new List<int>();
+        List<int> le = new List<int>();
+        foreach (int x in arr)
+        {
+            if (x % 2 == 0) chan.Add(x);
+            else le.Add(x);
+        }
+        Console.WriteLine(string.Join(" ", chan));
+        Console.WriteLine(string.Join(" ", le));
+    }
+
+    static void Bai10_SapXepTangGiam(int[] arr)
+    {
+        Array.Sort(arr);
+        Console.WriteLine(string.Join(" ", arr));
+
+        int[] giam = (int[])arr.Clone();
+        Array.Reverse(giam);
+        Console.WriteLine(string.Join(" ", giam));
+    }
+
+    static void Bai11_ChenPhanTu(int[] arr, int x)
+    {
+        int n = arr.Length;
+        int[] newArr = new int[n + 1];
+        int i = 0, j = 0;
+        bool daChen = false;
+
+        while (i < n)
+        {
+            if (!daChen && x < arr[i])
+            {
+                newArr[j++] = x;
+                daChen = true;
+            }
+            newArr[j++] = arr[i++];
+        }
+        if (!daChen) newArr[j] = x;
+
+        Console.WriteLine(string.Join(" ", newArr));
+    }
+
+    static void Bai12_XoaPhanTu(int[] arr, int k)
+    {
+        if (k < 0 || k >= arr.Length) return;
+        int[] newArr = new int[arr.Length - 1];
+        int j = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i == k) continue;
+            newArr[j++] = arr[i];
+        }
+        Console.WriteLine(string.Join(" ", newArr));
+    }
+
+    static void Bai13_DemTanSuat(int[] arr)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        foreach (int x in arr)
+        {
+            if (dic.ContainsKey(x)) dic[x]++;
+            else dic[x] = 1;
+        }
+        foreach (var item in dic)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+
+    static void Bai14_XuatHienNhieuNhat(int[] arr)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        foreach (int x in arr)
+        {
+            if (dic.ContainsKey(x)) dic[x]++;
+            else dic[x] = 1;
+        }
+
+        int maxFreq = 0;
+        int res = int.MaxValue;
+
+        foreach (var item in dic)
+        {
+            if (item.Value > maxFreq)
+            {
+                maxFreq = item.Value;
+                res = item.Key;
+            }
+            else if (item.Value == maxFreq)
+            {
+                if (item.Key < res) res = item.Key;
+            }
+        }
+        Console.WriteLine(res);
+    }
+}
---
+++ 2chieuexcersises7.cs
@@ -0,0 +1,326 @@
+﻿using System;
+
+class MangHaiChieu
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Nhập số hàng m và số cột n: ");
+        string[] size = Console.ReadLine().Split(' ');
+        int m = int.Parse(size[0]);
+        int n = int.Parse(size[1]);
+        int[,] matrix = NhapMaTran(m, n);
+        Bai21_XuatMaTran(matrix);
+
+        Bai22_TongDongCot(matrix);
+
+        Bai23_MaxVaViTri(matrix);
+
+        
+        if (m == n)
+        {
+            Bai24_DuongCheoChinh(matrix);
+
+            Bai25_TongDuongCheoPhu(matrix);
+        }
+        else
+        {
+            Console.WriteLine("\n[Lưu ý] Các bài 24, 25 yêu cầu Ma trận vuông (m phải bằng n).");
+        }
+
+        Console.Write("Nhập số X cần tìm: ");
+        int x26 = int.Parse(Console.ReadLine());
+        Bai26_TimKiemX(matrix, x26);
+
+        Bai27_DemAmBien(matrix);
+
+        Console.WriteLine($"Mời nhập tiếp ma trận B cùng kích thước {m}x{n}:");
+        int[,] matrixB = NhapMaTran(m, n);
+        Bai28_CongHaiMaTran(matrix, matrixB);
+
+        Bai29_MaTranChuyenVi(matrix);
+
+        if (m == n)
+        {
+            Bai30_KiemTraDoiXung(matrix);
+        }
+
+        Bai31_SapXepTungDong(matrix);
+
+        Console.Write("Nhập 2 chỉ số dòng r1 và r2 cần đổi chỗ: ");
+        string[] rows = Console.ReadLine().Split(' ');
+        int r1 = int.Parse(rows[0]);
+        int r2 = int.Parse(rows[1]);
+        Bai32_DoiChoHaiDong(matrix, r1, r2);
+
+        if (m == n)
+        {
+            Bai33_MatrixTamGiacTren(matrix);
+        }
+
+        Bai34_DongTongLonNhat(matrix);
+    }
+
+    static int[,] NhapMaTran(int m, int n)
+    {
+        int[,] matrix = new int[m, n];
+        for (int i = 0; i < m; i++)
+        {
+            string[] tokens = Console.ReadLine().Split(' ');
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = int.Parse(tokens[j]);
+            }
+        }
+        return matrix;
+    }
+
+    static void Bai21_XuatMaTran(int[,] matrix)
+    {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Bai22_TongDongCot(int[,] matrix)
+    {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        int[] tongDong = new int[m];
+        int[] tongCot = new int[n];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                tongDong[i] += matrix[i, j];
+                tongCot[j] += matrix[i, j];
+            }
+        }
+        Console.WriteLine(string.Join(" ", tongDong));
+        Console.WriteLine(string.Join(" ", tongCot));
+    }
+
+    static void Bai23_MaxVaViTri(int[,] matrix)
+    {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        int maxVal = matrix[0, 0];
+        int hang = 0, cot = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i, j] > maxVal)
+                {
+                    maxVal = matrix[i, j];
+                    hang = i;
+                    cot = j;
+                }
+            }
+        }
+        Console.WriteLine($"{maxVal} tại hàng {hang}, cột {cot}");
+    }
+
+    static void Bai24_DuongCheoChinh(int[,] matrix)
+    {
+        int n = matrix.GetLength(0);
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(matrix[i, i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Bai25_TongDuongCheoPhu(int[,] matrix)
+    {
+        int n = matrix.GetLength(0);
+        int tong = 0;
+        for (int i = 0; i < n; i++)
+        {
+            tong += matrix[i, n - 1 - i];
+        }
+        Console.WriteLine(tong);
+    }
+
+    static void Bai26_TimKiemX(int[,] matrix, int x)
+    {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        bool found = false;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i, j] == x)
+                {
+                    Console.Write($"({i},{j}) ");
+                    found = true;
+                }
+            }
+        }
+        if (!found) Console.Write("NOT FOUND");
+        Console.WriteLine();
+    }
+
+    static void Bai27_DemAmBien(int[,] matrix)
+    {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        int count = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1)
+                {
+                    if (matrix[i, j] < 0) count++;
+                }
+            }
+        }
+        Console.WriteLine(count);
+    }
+
+    static void Bai28_CongHaiMaTran(int[,] matrixA, int[,] matrixB)
+    {
+        int m = matrixA.GetLength(0);
+        int n = matrixA.GetLength(1);
+        int[,] matrixC = new int[m, n];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrixC[i, j] = matrixA[i, j] + matrixB[i, j];
+                Console.Write(matrixC[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Bai29_MaTranChuyenVi(int[,] matrix)
+    {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        int[,] AT = new int[n, m];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                AT[i, j] = matrix[j, i];
+                Console.Write(AT[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Bai30_KiemTraDoiXung(int[,] matrix)
+    {
+        int n = matrix.GetLength(0);
+        bool doiXung = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (matrix[i, j] != matrix[j, i])
+                {
+                    doiXung = false;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine(doiXung ? "YES" : "NO");
+    }
+
+    static void Bai31_SapXepTungDong(int[,] matrix)
+    {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        int[,] tempMatrix = (int[,])matrix.Clone();
+
+        for (int i = 0; i < m; i++)
+        {
+            int[] row = new int[n];
+            for (int j = 0; j < n; j++) row[j] = tempMatrix[i, j];
+
+            Array.Sort(row);
+
+            for (int j = 0; j < n; j++)
+            {
+                tempMatrix[i, j] = row[j];
+                Console.Write(tempMatrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Bai32_DoiChoHaiDong(int[,] matrix, int r1, int r2)
+    {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        if (r1 < 0 || r1 >= m || r2 < 0 || r2 >= m) return;
+
+        int[,] tempMatrix = (int[,])matrix.Clone();
+        for (int j = 0; j < n; j++)
+        {
+            int temp = tempMatrix[r1, j];
+            tempMatrix[r1, j] = tempMatrix[r2, j];
+            tempMatrix[r2, j] = temp;
+        }
+        Bai21_XuatMaTran(tempMatrix);
+    }
+
+    static void Bai33_MatrixTamGiacTren(int[,] matrix)
+    {
+        int n = matrix.GetLength(0);
+        bool laTamGiacTren = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++) // Xét các phần tử nằm phía dưới đường chéo chính (j < i)
+            {
+                if (matrix[i, j] != 0)
+                {
+                    laTamGiacTren = false;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine(laTamGiacTren ? "YES" : "NO");
+    }
+
+    static void Bai34_DongTongLonNhat(int[,] matrix)
+    {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        int maxTong = int.MinValue;
+        int chiSoDong = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            int tongDong = 0;
+            for (int j = 0; j < n; j++)
+            {
+                tongDong += matrix[i, j];
+            }
+            if (tongDong > maxTong)
+            {
+                maxTong = tongDong;
+                chiSoDong = i;
+            }
+        }
+        Console.WriteLine($"Dòng {chiSoDong} có tổng lớn nhất là {maxTong}");
+    }
+}
